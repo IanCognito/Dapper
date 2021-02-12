9a6c57628a70577a53bde5f466c27410e0bb2085
Chris Randle <chris@torpedotv.com>
2021-02-12 11:02:56 +0000

    Finished (3.3) Getting Started > Repository Skeleton

--- TTV.Dapper/DataLayer/IContactRepository.cs
+++ TTV.Dapper/DataLayer/IContactRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DataLayer
+﻿using System.Collections.Generic;
+
+namespace DataLayer
 {
 	public interface IContactRepository
 	{
+		Contact Find(int id);
+		List<Contact> GetAll();
+		Contact Add(Contact contact);
+		Contact Update(Contact contact);
+		void Remove(int id);
 	}
 }
--- TTV.Dapper/Runner/Program.cs
+++ TTV.Dapper/Runner/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using DataLayer;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Runner
 {
 	class Program
 	{
+		private static IConfigurationRoot config;
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+		}
+
+		private static void Initialize()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+			config = builder.Build();
+		}
+
+		private static IContactRepository CreateRepository()
+		{
+			return new ContactRepository(config.GetConnectionString("DefaultConnection"));
 		}
 	}
 }
